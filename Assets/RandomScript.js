2ade546bc7314428a4ddd5ec267f0e94e0387f1c
Mark Ang <ang1990@hotmail.com>
2014-02-22 21:16:49 +0800

    Random Script stops zombies' movement after 2.5s

---
+++ Assets/RandomScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var mover : ZombieMover;
+var t : float;
+
+function Start () {
+t = Time.time;
+mover = GetComponent(ZombieMover);
+}
+
+function Update () {
+if(Time.time > t + 2.5)
+	{
+		mover.speed = 0.5 * mover.speed; 
+	}
+}
