34fa5d84aa6b09c86f5e88adf52d5f17b1d77065
ang1990 <ang1990@hotmail.com>
2014-03-25 12:59:03 +0800

    Move Speed Perk added.
    Perk is currently added by pressing the 'm' key. 'm' for move, you see.
    
    The perk multiplies movespeed by a factor of 1.25. I've yet to figure
    out a good way to put the perk on the Inspector so that we can play with
    the numbers, but for now the factor is hardcoded.
    
    PerkInitializer also holds an int recording the number of skill points.
    We can get the script to call zedResources for this number instead.
    
    The good news is that this concept looks quite extendable to include
    upgrades to weapon damage, etc.

---
+++ Assets/Scripts/Environment/PerkInitializer.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var zed : GameObject;
+var skillPointsLeft : int = 2;
+
+function Start () {
+zed = GameObject.Find("zed");
+}
+
+function Update () {
+	if(Input.GetKey("m")) {
+		if(zed.GetComponent(PerkAddMoveSpeed1) == null) {
+			zed.AddComponent(PerkAddMoveSpeed1);
+			skillPointsLeft -= zed.GetComponent(PerkAddMoveSpeed1).skillPointCost;
+			}
+	}
+}
---
+++ Assets/Scripts/Zed/PerkAddMoveSpeed1.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var zed : GameObject;
+var factor : float = 1.25;
+var skillPointCost : int = 1;
+
+function Start () {
+setPerk();
+}
+
+function setPerk () {
+
+// If zed already has this perk, don't do anything and return a false to
+// indicate that zed already has the perk. If not, add the perk and set
+// its effect on zed.
+
+if(transform == GameObject.Find("zed").transform) {
+	zed = GameObject.Find("zed");
+	var zedMovement : ZedMovement = zed.GetComponent("ZedMovement") as ZedMovement;
+	zedMovement.maxSpeed *= factor;
+	}
+}
