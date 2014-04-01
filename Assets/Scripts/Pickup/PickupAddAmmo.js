face35e73f5d196dbf5e291ef312381a83281c05
ang1990 <ang1990@hotmail.com>
2014-04-01 11:50:57 +0800

    Pickups finally working.
    NOTE: Zed is no longer kinematic. This will affect the laser's
    collision.
    
    Suggested to use IgnoreCollision as a solution for this.

--- Assets/Scripts/Pickup/PickupAddAmmo.js
+++ Assets/Scripts/Pickup/PickupAddAmmo.js
@@ -4,19 +4,19 @@ var numberOfClips : int;
 var zedResources : ZedResources;
 
 function Start () {
-	numberOfClips = Mathf.FloorToInt(Random.value * 3);
+	numberOfClips = 1;
 	zedResources = GameObject.Find("zed").GetComponent(ZedResources) as ZedResources;
 }
 
 function OnTriggerEnter2D(otherCollider : Collider2D) {
-	if (otherCollider.gameObject.Equals(GameObject.Find("zed"))) {
+	if (otherCollider.CompareTag("Player")) {
 		Debug.Log("Collides!");
-		gameObject.collider2D.enabled = false;
 		zedResources = otherCollider.gameObject.GetComponent(ZedResources) as ZedResources;
-		for (var weapon in zedResources.weapons) {
+		for(var weapon in zedResources.weapons) {
 			weapon.addClips(numberOfClips);
 		}
+		collider2D.enabled = false;
+		WaitForEndOfFrame();
+		Destroy(gameObject);
 	}
-	WaitForEndOfFrame();
-	Destroy(this);
 }
