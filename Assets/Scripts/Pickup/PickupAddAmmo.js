59e465960555962fa2844c6eef673c53ebbbf025
ang1990 <ang1990@hotmail.com>
2014-04-03 17:34:06 +0800

    Pickup works properly again.

--- Assets/Scripts/Pickup/PickupAddAmmo.js
+++ Assets/Scripts/Pickup/PickupAddAmmo.js
@@ -3,18 +3,26 @@
 var numberOfClips : int;
 var zedResources : ZedResources;
 var pickupBounds : Bounds;
-var pickupLength : float;
+var spriteLength : float;
+var distanceFromZed : float;
 
 function Start () {
 	numberOfClips = 1;
 	zedResources = GameObject.Find("zed").GetComponent(ZedResources) as ZedResources;
-	pickupBounds = gameObject.GetComponent(MeshRenderer).bounds;
-	pickupLength = pickupBounds.max.x - pickupBounds.min.x;
+	pickupBounds = gameObject.GetComponent(SpriteRenderer).bounds;
+	spriteLength = pickupBounds.max.x - pickupBounds.min.x;
+	//transform.position.z = GameObject.Find("zed").transform.position.z;
 }
 
 function Update() {
-	if(pickupBounds.SqrDistance(GameObject.Find("zed").transform.position) < pickupLength*pickupLength)
+	var zedPosition = GameObject.Find("zed").transform.position;
+	zedPosition.z = transform.position.z;
+	if (Vector3.Magnitude(zedPosition - transform.position) < spriteLength) {
 		pickupExecute();
+	}
+	
+//	if(pickupBounds.SqrDistance() < spriteLength*spriteLength)
+//		pickupExecute();
 }
 
 function pickupExecute() {
